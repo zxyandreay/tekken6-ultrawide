15d8e3b6bb96d514918778505e5d59c8ba0abe31
/ˈʒæn.dreɪ/ <zxyandreay@gmail.com>
2026-09-13 16:55:07 +0800

    build: package camera CWCheats with plugin

--- tools/package_plugin.py
+++ tools/package_plugin.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import argparse
 import zipfile
 
-PREFIX = "PSP/PLUGINS/Tekken6Ultrawide"
+PLUGIN_PREFIX = "PSP/PLUGINS/Tekken6Ultrawide"
+CHEAT_PATH = "PSP/Cheats/ULUS10466.ini"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--prx", required=True, type=Path)
@@ -13,7 +14,8 @@ args = parser.parse_args()
 root = Path(__file__).parents[1]
 args.output.parent.mkdir(parents=True, exist_ok=True)
 with zipfile.ZipFile(args.output, "w", zipfile.ZIP_DEFLATED) as z:
-    z.write(args.prx, f"{PREFIX}/Tekken6Ultrawide.prx")
-    z.write(root / "plugin/config/plugin.ini", f"{PREFIX}/plugin.ini")
+    z.write(args.prx, f"{PLUGIN_PREFIX}/Tekken6Ultrawide.prx")
+    z.write(root / "plugin/config/plugin.ini", f"{PLUGIN_PREFIX}/plugin.ini")
+    z.write(root / "ULUS10466.ini", CHEAT_PATH)
     z.write(root / "README-PLUGIN.md", "README-PLUGIN.md")
 print(args.output)
